c2ed5f8956143b74efb0cebfa46a1aeec50af31d
Guilherme Thales <guilherme.thales@smarttbotter.com>
2020-06-07 23:15:03 -0300

    adds items listing api

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,18 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('', (request, response) => {
-  return response.json({ message: 'Hello world' });
+routes.get('/items', async (request, response) => {
+  const items = await knex('items').select('*');
+
+  const serializedItems = items.map((item) => ({
+    id: item.id,
+    name: item.title,
+    image_url: `http://localhost:3333/uploads/${item.image}`,
+  }))
+
+  return response.json(serializedItems);
 });
 
 export default routes;
