9a8f14bc7bc1008c1b7e3def812e550c685abb6a
Guilherme Thales <guilherme.thales@smarttbotter.com>
2020-06-07 22:17:50 -0300

    adds config to database

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  console.log('Users listing');
-
-  response.json([
-    'Guilherme',
-    'Diego',
-    'Robson',
-    'Daniel'
-  ]);
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
