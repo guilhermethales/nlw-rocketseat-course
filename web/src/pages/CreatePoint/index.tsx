6a7737c58fa38cf245099ee043c4f48f1132034f
Guilherme Thales <guilherme.thales@smarttbotter.com>
2020-06-11 22:40:32 -0300

    searchs states and cities from IBGE api

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi'
 import { Map, TileLayer, Marker } from 'react-leaflet';
-import api from '../../services/api';
+import api, { IBGE_STATE_API } from '../../services/api';
 
 import './styles.css';
 import logo from '../../assets/logo.svg';
@@ -13,14 +14,59 @@ interface Item {
   image_url: string
 };
 
+interface IBGEUFResponse {
+  sigla: string,
+};
+
+interface IBGECityResponse {
+  nome: string,
+};
+
 const CreatePoint = () => {
   const [items, setItems] = useState<Item[]>([]);
+  const [ufs, setUfs] = useState<string[]>([]);
+  const [cities, setCities] = useState<string[]>([]);
+  const [selectedUf, setSelectedUf] = useState('0');
+  const [selectedCity, setSelectedCity] = useState('0');
 
   useEffect(() => {
     api.get('/items').then((response) => {
         setItems(response.data);
       })
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    axios.get<IBGEUFResponse[]>(IBGE_STATE_API)
+      .then((response) => {
+        const ufInitials = response.data.map(uf => uf.sigla);
+
+        setUfs(ufInitials);
+      });
+  }, []);
+
+  useEffect(() => {
+    if(selectedUf !== '0') {
+      axios.get<IBGECityResponse[]>(`${IBGE_STATE_API}/${selectedUf}/municipios`)
+        .then((response) => {
+          const cityNames = response.data.map(city => city.nome);
+
+          setCities(cityNames);
+        });
+    }
+  }, [selectedUf]);
+
+  function handleSelectedUf(event:ChangeEvent<HTMLSelectElement>) {
+    const uf = event.target.value;
+
+    setSelectedUf(uf);
+  };
+
+  function handleSelectedCity(event:ChangeEvent<HTMLSelectElement>) {
+    const city = event.target.value;
+
+    setSelectedCity(city);
+  };
+
   return (
     <div id="page-create-point">
       <header>
@@ -87,14 +133,30 @@ const CreatePoint = () => {
           <div className="field-group">
             <div className="field">
               <label htmlFor="uf">Estado (UF)</label>
-              <select name="uf" id="uf">
+              <select 
+                name="uf" 
+                id="uf" 
+                value={selectedUf} 
+                onChange={handleSelectedUf}
+              >
                 <option value="0">Selecione uma UF</option>
+                {ufs.map(uf => (
+                  <option key={uf} value={uf}>{uf}</option>
+                ))}
               </select>
             </div>
             <div className="field">
               <label htmlFor="city">Cidade</label>
-              <select name="city" id="city">
+              <select 
+                name="city" 
+                id="city"
+                value={selectedCity}
+                onChange={handleSelectedCity}
+              >
                 <option value="0">Selecione uma cidade</option>
+                {cities.map(city => (
+                  <option key={city} value={city}>{city}</option>
+                ))}
               </select>
             </div>
           </div>
--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+export const IBGE_STATE_API = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
+
 const api = axios.create({
   baseURL: 'http://localhost:3333'
 });
